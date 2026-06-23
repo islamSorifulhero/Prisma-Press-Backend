7006bc10608ff1dc69f42fa888f55f685de79a44
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-23 23:35:08 +0600

    Configuration and dotenv file updated

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,23 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config"
-import {userRoutes} from "./modules/user/user.route";
+import { userRoutes } from "./modules/user/user.route";
 
 const app: Application = express();
 
+app.use(cors({
+    origin: config.app_url,
+    credentials: true,
+}))
+
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
-})
+});
 
 // app.post()
 
