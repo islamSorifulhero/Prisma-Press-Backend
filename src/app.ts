8a019725fa712848e69017b2144c90a3f9a42057
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-23 22:17:38 +0600

    setting up server and app file added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import config from "./config"
+import {userRoutes} from "./modules/user/user.route";
 
 const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World!");
 })
 
+// app.post()
+
+app.use("/api/users", userRoutes)
 export default app;
