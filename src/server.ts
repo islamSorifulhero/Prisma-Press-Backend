28880229b5e3ba409959aa7e6d2ba0fab55090b8
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-23 11:34:30 +0600

    server prisma connected completed

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import app from "./app";
+import "dotenv/config";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT || 5000;
+const PORT = config.port;
 
 async function main() {
     try {
+        await prisma.$connect();
+        console.log("Connect to the database successfully.");
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
     } catch (error) {
         console.error("Error starting the server:", error);
+        await prisma.$disconnect();
         process.exit(1);
     }
 }
