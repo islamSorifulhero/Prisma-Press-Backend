5207daff1e8330239fdcf3fd07dbd3528a8c1814
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-25 07:02:56 +0600

    user controller status added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from "express";
-import httpStatus from "http-status";
+// import { HttpStatus } from "http-status";
+import httpStatus from "http-status"
 import { userService } from "./user.service";
+import { Request, Response } from "express";
 
 const registerUser = async (req: Request, res: Response) => {
     try {
         const payload = req.body;
 
         const user = await userService.registerUserIntoDB(payload);
-
         res.status(httpStatus.CREATED).json({
             success: true,
             statusCode: httpStatus.CREATED,
-            message: "User registered successfully",
+            message: "User registerd successfully",
             data: {
                 user
             }
@@ -25,7 +25,6 @@ const registerUser = async (req: Request, res: Response) => {
             message: "Failed to register user",
             error: (error as Error).message
         })
-
     }
 }
 
