5b5c984e80e6cb196f5f726b31b644ca18ef8afd
MD Soriful Islam <islamsoriful.hero@gmail.com>
2026-06-30 23:59:09 +0600

    authorization startsWith and split added

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,9 +1,11 @@
 
 
-
+import { NextFunction, Request, Response } from "express";
+import { Role } from "../../generated/prisma/enums"
+import { catchAsync } from "../utils/catchAsync";
 
 declare global {
-    namespace Express{
+    namespace Express {
         interface Request {
             user?: {
                 email: string;
@@ -13,4 +15,19 @@ declare global {
             }
         }
     }
+}
+
+
+// auth(Role.ADMIN, Role.USER, Role.Author)
+// auth() => ... requiredRoles => [Role.ADMIN, Role.USER, Role.AUTHOR]
+
+export const auth = (...requiredRoles: Role[]) => {
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies.accessToken ?
+            req.cookies.accessToken : req.headers.authorization?.startsWith("Bearer") ?
+                req.headers.authorization?.split(" ")[1]
+                : req.headers.authorization;
+
+                
+    })
 }
